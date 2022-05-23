abcc58de42ac5d505a0223b662da0a12e3583bc3
SergeyZhilin93 <solomonsdt93@gmail.com>
2022-05-23 17:18:59 +0300

    tested react-routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import {
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import LoginPage from './Components/login'
+import Registration from './Components/Registration' 
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <div>
+        <Link to='/'>Login Page </Link>
+        <Link to='/registration'>Registration</Link>
+      </div>
+      <Routes>
+       <Route path="/" element={ <LoginPage/> }/>
+       <Route path="/registration" element={ <Registration/> }/>
+      </Routes>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <div>
-      <h1>Заголовок</h1>
-      <p>Проверка</p>
-    </div>
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
 );
