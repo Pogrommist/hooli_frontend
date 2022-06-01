852e13c7fa2d9497f4b837be419980108b370bc4
SergeyZhilin93 <solomonsdt93@gmail.com>
2022-06-01 18:26:01 +0300

    add axios, validator to registration and login page WIP

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import { useForm } from "react-hook-form";
 import { Link } from 'react-router-dom'
+import { axiosInstance } from '../../services/axios'
+import './style.scss'
 
 export default function LoginPage() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => axiosInstance.post('users/sign_in', { user: data })
+  
   return (
-    <div>
-      <form>
-        <input type='email' name='e-mail'/>
-        <input type='password' name='password'/>
+    <div className='site-background'>
+      <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
+        <input placeholder='E-mail' type='email' {...register('email', { required: true })}/>
+        {errors.email && <span>Please enter a valid E-mail address.</span>}
+        <input placeholder='Password' type='password' {...register('password', { required: true })}/>
         <button>Submit</button>
         <div>
           <Link to='/registration'>Registration</Link>
--- src/components/Registration/index.js
+++ src/components/Registration/index.js
@@ -1,17 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
-import axios from 'axios';
-import validator from 'validator';
 import { Link } from "react-router-dom";
+import { axiosInstance } from '../../services/axios'
 import "./style.scss";
 
 export default function Registration() {
   const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data)
-
+  const onSubmit = data => axiosInstance.post('users/', { user: data })
+  
   return (
-    <div className="test">
-      <form onSubmit={handleSubmit(onSubmit)}>
+    <div className='site-background'>
+      <form className="registration-form" onSubmit={handleSubmit(onSubmit)}>
         <input placeholder='First name' type='text' {...register('first_name', { required: true })}/>
         <input placeholder='Last name' type='text' {...register('last_name', { required: true })}/>
         <input placeholder='E-mail' type='email' {...register('email', { required: true })}/>
