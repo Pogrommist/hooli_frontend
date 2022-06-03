41755dc2e8872791c1985bc0bc9166e1e2fb28f8
Alexey Arbuzov <91293alex@gmail.com>
2022-06-03 03:12:09 +0300

    add styles and validation to registration page

--- src/components/Registration/index.js
+++ src/components/Registration/index.js
@@ -2,23 +2,43 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import { axiosInstance } from '../../services/axios'
+import Logo from '../../assets/images/logo.svg'
 import "./style.scss";
+
+// ToDo: Move the input and it's styles into 'shared' folder
+
+const Input = ({ label, register, required, type = 'text', name, className = 'registration-form__input', placeholder, hasError }) => (
+  <>
+    { label && <label>{label}</label>}
+    <input className={`${className} ${hasError ? `${className}--invalid` : ''}`} type={type} {...register(name, { required })} placeholder={placeholder}/>
+  </>
+);
 
 export default function Registration() {
   const { register, handleSubmit, formState: { errors } } = useForm();
   const onSubmit = data => axiosInstance.post('users/', { user: data })
-  
+
   return (
     <div className='site-background'>
       <form className="registration-form" onSubmit={handleSubmit(onSubmit)}>
-        <input placeholder='First name' type='text' {...register('first_name', { required: true })}/>
-        <input placeholder='Last name' type='text' {...register('last_name', { required: true })}/>
-        <input placeholder='E-mail' type='email' {...register('email', { required: true })}/>
-        {errors.email && <span>Please enter a valid E-mail address.</span>}
-        <input placeholder='Password' type='password' {...register('password', { required: true })}/>
-        <button>Submit</button>
-        <div>
-          <Link to='/'>Login Page </Link>
+        <img src={Logo} className="registration-form__logo" />
+        <div className="registration-form__inputs">
+          <Input name="first_name" required register={register} placeholder="First name" hasError={errors.first_name} />
+          <Input name="last_name" required register={register} placeholder="Last name" hasError={errors.last_name} />
+          <Input type="email" name="email" required register={register} placeholder="E-mail" hasError={errors.email} />
+          <Input type="password" name="password" required register={register} placeholder="Password" hasError={errors.password} />
+        </div>
+        <div className="registration-form__actions">
+          <div className="registration-form__action_item">
+            <input type="checkbox" className="registration-form__action_item__checkbox" />
+            <span className="registration-form__action_item__link">I agree witn <a href="#">terms and conditions</a></span>
+          </div>
+          <div className="registration-form__action_item">
+            <button className="registration-form__action_item__button">Sign Up</button>
+          </div>
+          <div className="registration-form__action_item">
+          <span className="registration-form__action_item__link">Alreasdy have account? <Link to='/'>Sign in </Link></span>
+          </div>
         </div>
       </form>
     </div>
