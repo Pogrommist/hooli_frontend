3d2d4b7f4566ae001b9046339e4cad5996a825c7
SergeyZhilin93 <solomonsdt93@gmail.com>
2022-06-09 18:16:04 +0300

    made refactor styles

--- src/components/Registration/RegistrationFormActions/index.js
+++ src/components/Registration/RegistrationFormActions/index.js
@@ -3,9 +3,8 @@ import { Link } from "react-router-dom";
 
 export function RegistrationFormActions() {
 	return(
-		<>
-			<div className="base-form__actions">
-				<div className="base-form__action_item">
+		<div className="base-form__actions">
+			<div className="base-form__action_item">
 				<input type="checkbox" className="base-form__action_item__checkbox" />
 				<span className="base-form__action_item__link">I agree witn <a href="#">terms and conditions</a></span>
 			</div>
@@ -17,7 +16,6 @@ export function RegistrationFormActions() {
 			<div className="base-form__action_item">
 				<span className="base-form__action_item__link">Already have account? <Link to='/'>Sign in </Link></span>
 			</div>
-			</div>
-		</>
+		</div>
 	)
 }
